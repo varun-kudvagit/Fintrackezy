9621a75005b6241ed5c1e045e260ff419d70da3c
Varun Kudva <varunkudva111@gmail.com>
2025-04-29 17:58:19 +0530

    Create expenseSlice.js

---
+++ frontend/src/redux/expenseSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const expenseSlice = createSlice({
+    name:"expense",
+    initialState:{
+        category:"",
+        markAsDone:"",
+        expenses:[],
+        singleExpense:null
+    },
+    reducers: {
+        setCategory: (state,action) => {
+            state.category =  action.payload
+        },
+        setMarkAsDone: (state,action)=> {
+            state.markAsDone = action.payload
+        },
+        setExpenses: (state,action)=> {
+            state.expenses = action.payload
+        },
+        setSingleExpense: (state,action) => {
+            state.singleExpense = action.payload
+        }
+    }
+})
+
+export const {
+    setCategory,
+    setMarkAsDone,
+    setExpenses,
+    setSingleExpense
+} = expenseSlice.actions;
+
+export default expenseSlice.reducer
