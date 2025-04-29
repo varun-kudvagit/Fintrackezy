c305531de9d8b0adb177ed36220dfa41a7be81da
Varun Kudva <varunkudva111@gmail.com>
2025-04-30 00:49:53 +0530

    Update postcss.config.js

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,7 +1,7 @@
 
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+export default {
+  plugins: [tailwindcss, autoprefixer],
 };
