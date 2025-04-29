e6ab2f4e78e7707843e5d94948896d199ab19fa6
Varun Kudva <varunkudva111@gmail.com>
2025-04-30 03:43:35 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite';
 
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  base: '/frontend/',  
-  build: {
-    outDir: path.resolve(__dirname, 'frontend/build'),  
-  },
-});
-
+  plugins: [react(), tailwindcss()],
+})
