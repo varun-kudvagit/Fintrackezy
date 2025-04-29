ae907f9c1b555a9ec62503751083d927df12a62e
Varun Kudva <varunkudva111@gmail.com>
2025-04-29 17:57:37 +0530

    Create authSlice.js

---
+++ frontend/src/redux/authSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+    name:'auth',
+    initialState:{
+        loading:false,
+        user:null
+    },
+    reducers:{
+        //actions
+        setLoading: (state, action) => {
+            state.loading = action.payload
+        },
+        setAuthUser: (state,action) => {
+            state.user = action.payload;
+        }
+    }
+})
+
+export const{
+    setLoading,
+    setAuthUser
+} = authSlice.actions;
+
+export default authSlice.reducer
