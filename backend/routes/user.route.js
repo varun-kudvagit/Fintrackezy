6d8a84ccfc5a59a9738a3f34c423a9bad172776a
Varun Kudva <varunkudva111@gmail.com>
2025-04-30 13:27:32 +0530

    Update user.route.js

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,5 @@
 import express from "express";
-import { login, logout, register } from "../controllers/user.controller.js";
-
-const router = express.Router();
-
-router.route("/register").post(register)
-router.route("/login").post(login)
-router.route("/logout").get(logout)
-
-export default router
+import getUserInfoController from "../controllers/user.contoller.js";
+const userRouter = express.Router();
+userRouter.get("/", getUserInfoController);
+export default userRouter;
