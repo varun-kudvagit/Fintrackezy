d7c01db14d83e8e9d85e60e2cbc6a75e355e90d4
Varun Kudva <varunkudva111@gmail.com>
2025-04-30 18:07:42 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,52 @@
-import './App.css'
-import Home from './components/Home'
-import Login from './components/Login'
-import Signup from './components/Signup'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { Toaster } from 'sonner'
+import { useEffect } from "react";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+import Home from "./pages/Dashboard/Home";
+import Income from "./pages/Dashboard/Income";
+import Expense from "./pages/Dashboard/Expense";
+import AuthLayout from "./layouts/AuthLayout";
+import ProtectedRoute from "./layouts/ProtectedRoute";
+import useAuthStore from "./store/authStore";
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-const appRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />
-  },
-  {
-    path: "/login",
-    element: <Login />
-  },
-  {
-    path: "/signup",
-    element: <Signup />
-  }
-])
+const App = () => {
+  // Initialize Auth store in App start
+  const initializeAuth = useAuthStore((state) => state.initializeAuth);
+  useEffect(() => {
+    initializeAuth();
+  }, [initializeAuth]);
 
-function App() {
+
   return (
-    <div>
-      <RouterProvider router={appRouter} />
-      <Toaster />
-    </div>
-  )
-}
+    <>
+      <Router>
+        <Routes>
+          {/* Auth Routes */}
+          <Route path="/" element={<AuthLayout />}>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Route>
 
-export default App
+          {/* Protected App Routes */}
+          <Route path="/" element={<ProtectedRoute />}>
+            <Route path="/dashboard" element={<Home />} />
+            <Route path="/income" element={<Income />} />
+            <Route path="/expense" element={<Expense />} />
+          </Route>
+        <Route path="*" element={<div className="h-screen w-screen flex items-center justify-center text-4xl">Not Found</div>}></Route>
+        </Routes>
+      </Router>
+      <Toaster
+        toastOptions={{
+          className: "",
+          style: {
+            fontSize: "13px",
+          },
+        }}
+      />
+    </>
+  );
+};
+export default App;
