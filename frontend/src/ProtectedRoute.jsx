d7f0d09bb9267f77b237f97d8753958565c0ef04
Varun Kudva <varunkudva111@gmail.com>
2025-04-30 18:37:38 +0530

    Delete frontend/src/ProtectedRoute.jsx

--- frontend/src/ProtectedRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Outlet, Navigate } from "react-router-dom";
-import { getToken } from "../utils/helpers";
-const ProtectedRoute = () => {
-  const token = getToken();
-  return token ? <Outlet /> : <Navigate to="/login" />;
-};
-export default ProtectedRoute;
