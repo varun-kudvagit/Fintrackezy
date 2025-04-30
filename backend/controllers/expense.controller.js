6b61bc677d28ec5d4d2bd3de9abba8b23a50a383
Varun Kudva <varunkudva111@gmail.com>
2025-04-30 13:08:07 +0530

    Update expense.controller.js

--- backend/controllers/expense.controller.js
+++ backend/controllers/expense.controller.js
@@ -1,123 +1,60 @@
-import { Expense } from "../models/expenseModel.js";
-
-export const addExpense = async (req,res) => {
-    try {
-        const {description, amount, category} = req.body;
-        const userId = req.id //current loggedin userid
-        if(!description || !amount || !category){
-            return res.status(400).json({
-                message:"All fields are required",
-                success: false
-            })
-        }
-
-        const expense = await Expense.create({
-            description,
-            amount,
-            category,
-            userId
-        })
-        return res.status(201).json({
-            message:"New Expense added",
-            expense,
-            success:true
-        })
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-export const getAllExpense = async (req,res)=>{
-    try {
-        const userId = req.id 
-        let category = req.query.category || "";
-        const done = req.query.done || "";
-
-        const query = {
-            userId //filter by userId
-        }
-
-        if(category.toLowerCase() === "all"){
-            //no need to filter by category
-        }
-        else{
-            query.category = {$regex:category, $options:'i'}
-        }
-
-        if(done.toLowerCase() === "done"){
-            query.done = true
-        }
-        else if(done.toLowerCase() === "undone"){
-            query.done = false //filter for expense marked as undone/false
-        }
-
-        const expense = await Expense.find(query);
-
-        if(!expense || expense.length === 0){
-            return res.status(404).json({
-                message:"No expense found",
-                success:false
-            })
-        }
-        return res.status(200).json({
-            expense,
-            success:true
-        })
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-
-export const markAsDoneUndone = async (req,res) => {
-    try {
-        const expenseId = req.params.id;
-        const done = req.body;
-        const expense = await Expense.findByIdAndUpdate(expenseId, done, {new:true})
-
-        if(!expense){
-            return res.status(404).json({
-                message:"Expense not found",
-                success:false
-            })
-        }
-        return res.status(200).json({
-            message: `Expense marked as ${expense.done ? 'done' : 'undone'}`,
-            success:true
-        })
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-export const removeExpense = async (req,res) => {
-    try {
-        const expenseId = req.params.id;
-        await Expense.findByIdAndDelete(expenseId);
-        return res.status(200).json({
-            message: "Expense Removed",
-            success: true
-        })
-
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-export const updateExpense = async (req,res) => {
-    try {
-        const {description, amount, category} = req.body;
-        const expenseId = req.params.id;
-        const updateData = {description, amount, category}
-
-        const expense = await Expense.findByIdAndUpdate(expenseId, updateData, {new:true})
-        return res.status(200).json({
-            message:"Expense updated",
-            expense,
-            success:true
-        })
-
-    } catch (error) {
-        console.log(error)
-    }
-}
+import xlsx from "xlsx";
+import HTTPSTATUS from "../constants/http.constant.js";
+import {
+  addExpenseService,
+  getAllExpensesService,
+  deleteExpenseByIdService,
+} from "../services/expense.service.js";
+import { addExpenseSchema } from "../validation/expense.valiadtion.js";
+export const addExpenseController = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    addExpenseSchema.parse({ ...req.body, amount: Number(req.body.amount) });
+    let newlyAddedExpense = await addExpenseService(userId, req.body);
+    return res.status(HTTPSTATUS.CREATED).json({
+      newlyAddedExpense,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+export const getAllExpensesController = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    let expenses = await getAllExpensesService(userId);
+    return res.status(HTTPSTATUS.OK).json({
+      expenses,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+export const deleteExpenseController = async (req, res, next) => {
+  try {
+    let expenseId = req.params.id;
+    await deleteExpenseByIdService(expenseId);
+    return res.status(HTTPSTATUS.OK).json({
+      message: "Expense deleted successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+export const downloadInExcelController = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const expenses = await getAllExpensesService(userId);
+    const data = expenses.map((expense) => ({
+      Source: expense.source,
+      Amount: expense.amount,
+      Date: expense.date,
+    }));
+    const wb = xlsx.utils.book_new();
+    const ws = xlsx.utils.json_to_sheet(data);
+    xlsx.utils.book_append_sheet(wb, ws, "Expense");
+    xlsx.writeFile(wb, "expense_details.xlsx");
+    res.download("expense_details.xlsx");
+  } catch (error) {
+    next(error);
+  }
+};
