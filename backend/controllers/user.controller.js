e36617173a1225d92c228b57f6a683fbcac84df5
Varun Kudva <varunkudva111@gmail.com>
2025-04-30 13:08:46 +0530

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,97 +1,14 @@
-import { User } from "../models/userModel.js";
-import bcrypt from "bcryptjs"
-import jwt from "jsonwebtoken"
+import HTTPSTATUS from "../constants/http.constant.js";
+import getUserByIdService from "../services/user.service.js";
 
-
-export const register = async (req,res) => {
-    try {
-        const{fullname,email,password} = req.body;
-        if(!fullname || !email || !password){
-            return res.status(400).json({
-                message:"All fields are required",
-                success: false
-            })
-        }
-
-        const user = await User.findOne({email})
-        if(user){
-            return res.status(400).json({
-                message:"User already exists",
-                success: false
-            })
-        };
-
-
-        const hashedPassword = await bcrypt.hash(password , 10);
-        await User.create({
-            fullname,
-            email,
-            password: hashedPassword
-        })
-
-        return res.status(201).json({
-            message:"Account created Successfully",
-            success:true
-        })
-
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-export const login = async (req,res)=>{
-    try {
-        const {email,password} = req.body;
-        if(!email | !password){
-            return res.status(400).json({
-                message:"All fields are required",
-                success: false
-            })
-        }
-
-        const user = await User.findOne({email})
-        if(!user){
-            return res.status(400).json({
-                message:"Incorrect email or password",
-                success: false
-            })
-        };
-
-        const isPasswordMatch = await bcrypt.compare(password, user.password);
-        if(!isPasswordMatch){
-            return res.status(400).json({
-                message:"Incorrect email or password",
-                success: false
-            })
-        };
-
-        const tokenData = {
-            userId:user._id
-        }
-
-        const token = await jwt.sign(tokenData, process.env.SECRET_KEY, {expiresIn: '1d'});
-        return res.status(200).cookie('token', token, {maxAge:1*24*60*60*1000, httpOnly:true, sameSite: 'strict'}).json({
-            message:`Welcome back ${user.fullname}`,
-            user:{
-                _id:user._id,
-                fullname:user.fullname,
-                email:user.email
-            },
-            success:true
-        })
-
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-export const logout = async (req,res) => {
-    try {
-        return res.status(200).cookie("token","",{maxAge:0}).json({
-            message:" User logged out successfully",
-            success: true
-        })
-    } catch (error) {
-        console.log(error)
-    }
-}
+const getUserInfoController = async (req, res, next) => {
+  try {
+    let user = await getUserByIdService(req.user.id);
+    return res.status(HTTPSTATUS.OK).json({
+      user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+export default getUserInfoController;
