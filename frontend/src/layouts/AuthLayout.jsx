5fc9faffdd8680cd4c722e407f83b352fc0f4dea
Varun Kudva <varunkudva111@gmail.com>
2025-05-03 01:29:14 +0530

    Update AuthLayout.jsx

--- frontend/src/layouts/AuthLayout.jsx
+++ frontend/src/layouts/AuthLayout.jsx
@@ -7,10 +7,10 @@ const AuthLayout = () => {
   return (
     <div className="flex">
       <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
-        <h2 className="text-lg font-medium text-black">Expense Tracker</h2>
+        <h2 className="text-lg font-medium text-black">Fintrackezy</h2>
         <Outlet />
       </div>
-      <div className="hidden md:block w-[40vw] h-screen bg-voilet-50 bg-auth-bg-img bg-cover bg-no-repeat bg-center overflow-hidden p-8 relative">
+      <div className="hidden md:block w-[40vw] h-screen bg-violet-50 bg-auth-bg-img bg-cover bg-no-repeat bg-center overflow-hidden p-8 relative">
         <div className="w-48 h-48 rounded-[40px] bg-purple-600 absolute -top-7 -left-5"></div>
         <div className="w-48 h-56 rounded-[40px] border-[20px] border-fuchsia-600 absolute -top-[30%] -right-10"></div>
         <div className="w-48 h-48 rounded-[40px] bg-violet-500 absolute -bottom-7 -left-5"></div>
