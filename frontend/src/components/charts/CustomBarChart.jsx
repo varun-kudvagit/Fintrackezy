824bfbdcf5daad26156a823238bdbd8d5fe6f1e8
Varun Kudva <varunkudva111@gmail.com>
2025-05-03 01:51:19 +0530

    Update CustomBarChart.jsx

--- frontend/src/components/charts/CustomBarChart.jsx
+++ frontend/src/components/charts/CustomBarChart.jsx
@@ -25,7 +25,7 @@ const CustomBarChart = ({ data }) => {
           <p className="text-sm text-gray-600">
             Amount:{" "}
             <span className="text-sm font-medium text-gray-900">
-              ${payload[0].payload.amount}
+              ₹{payload[0].payload.amount}
             </span>
           </p>
         </div>
