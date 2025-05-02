48a50242df01ee3f6707d80a974f2725f07dab36
Varun Kudva <varunkudva111@gmail.com>
2025-05-03 01:50:39 +0530

    Update CustomLineChart.jsx

--- frontend/src/components/charts/CustomLineChart.jsx
+++ frontend/src/components/charts/CustomLineChart.jsx
@@ -19,7 +19,7 @@ const CustomLineChart = ({ data }) => {
           <p className="text-sm text-gray-600">
             Amount:{" "}
             <span className="text-sm font-medium text-gray-900">
-              ${payload[0].payload.amount}
+              ₹{payload[0].payload.amount}
             </span>
           </p>
         </div>
